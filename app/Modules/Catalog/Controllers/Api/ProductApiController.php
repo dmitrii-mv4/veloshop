93b96e9a56ab1e8bcf795068792b5bccf5143a7d
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2026-02-16 00:35:46 +0300

    product attributes

--- app/Modules/Catalog/Controllers/Api/ProductApiController.php
+++ app/Modules/Catalog/Controllers/Api/ProductApiController.php
@@ -3,16 +3,22 @@
 namespace App\Modules\Catalog\Controllers\Api;
 
 use App\Modules\Catalog\Models\Product;
+use App\Modules\Catalog\Resources\ProductCollection;
 use Illuminate\Http\Request;
 
 class ProductApiController
 {
     /**
+     * Количество постов блога на странице по-умолчанию.
+     */
+    private const int PRODUCTS_PER_PAGE = 100;
+
+    /**
      * Display a listing of the resource.
      */
-    public function index(): string
+    public function index()
     {
-        return Product::with(['attributes', 'offers'])->get()->toJson();
+        return ProductCollection::make(Product::paginate($this::PRODUCTS_PER_PAGE));
     }
 
     /**
