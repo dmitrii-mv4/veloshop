a3f89a1d04f69200a609e0b3372907688e326514
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2026-04-10 15:15:37 +0300

    api auth basic routes and controller

--- app/Modules/User/Controllers/Api/AuthController.php
+++ app/Modules/User/Controllers/Api/AuthController.php
@@ -3,7 +3,12 @@
 namespace App\Modules\User\Controllers\Api;
 
 use App\Core\Controllers\Controller;
+use App\Modules\User\Models\User;
 use App\Modules\User\Services\AuthService;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Контроллер для аутентификации через API
@@ -25,5 +30,48 @@ class AuthController extends Controller
         $this->authService = $authService;
     }
 
+    public function register(Request $request): JsonResponse
+    {
+        $feilds = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
 
+        $user = User::create($feilds);
+
+        event(new Registered($user));
+
+        return response()->json([
+            'user' => $user,
+        ]);
+
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $feilds = $request->validate([
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|min:8|'
+        ]);
+
+        if (!Auth::attempt($feilds)) {
+            return response()->json(['errors' => [
+                'user' => ['invalid credentials']
+            ]], 401);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json(['message' => 'Logged in successfully', 'user' => Auth::user()]);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(['message' => 'Logged out']);
+    }
 }
