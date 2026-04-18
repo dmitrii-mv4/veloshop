84bb0bdc72810b5af6b37c2c36fcbe71f2b7b2a0
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2026-04-18 17:05:59 +0300

    baskets functionality

--- app/Modules/Catalog/database/migrations/2025_03_12_000002_create_catalog_customers_table.php
+++ app/Modules/Catalog/database/migrations/2025_03_12_000002_create_catalog_customers_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Modules\Catalog\Models\CustomerType;
+use App\Modules\User\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,16 +12,14 @@ return new class extends Migration
     {
         Schema::create('catalog_customers', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->comment('ID пользователя (владелец профиля покупателя)');
-            $table->unsignedBigInteger('type_id')->comment('ID типа покупателя');
+            $table->foreignIdFor(User::class)->comment('ID пользователя (владелец профиля покупателя)');
+            $table->foreignIdFor(CustomerType::class)->comment('ID типа покупателя');
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->unsignedBigInteger('deleted_by')->nullable();
             $table->softDeletes();
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->foreign('type_id')->references('id')->on('catalog_customers_type')->restrictOnDelete();
             $table->foreign('created_by')->references('id')->on('users')->nullOnDelete();
             $table->foreign('updated_by')->references('id')->on('users')->nullOnDelete();
             $table->foreign('deleted_by')->references('id')->on('users')->nullOnDelete();
